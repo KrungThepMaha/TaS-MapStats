596a2caae17cd3b59d225d0cfc39714d28206028
shoffman <sean.hoffman@eleadcrm.com>
2015-09-02 15:20:08 -0500

    Basic Project Setup. Added in Main Plugin for use as a hook into the game. Also added in a basic GUI window for displaying stuff in the game.

---
+++ MapStatsChooser/GUIWindowMaptStatsChooser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using UnityEngine;
+
+namespace Plugin.KTMBobisback.MapStatsChooser
+{
+    /// <summary>
+    ///     The main GUI window for the map stats chooser.
+    /// </summary>
+    public class GUIWindowMapStatsChooser : MonoBehaviour
+    {
+        private readonly GUIManager guiMgr = GUIManager.getInstance();
+
+        //vars for displaying messages error and regular.
+        private static readonly Timer UpdateTimer = new Timer(5000);
+        private static bool _showErrorDialog = false;
+        private static string _messageTitle = "";
+        private static string _message = "";
+
+        /// <summary>
+        ///     This function is called once when this window starts up. 
+        ///     Do any one time setup/init things in this function.
+        /// </summary>
+        void Start()
+        {
+            UpdateTimer.Elapsed += UpdateDisplay;
+        }
+        
+        /// <summary>
+        ///     This is called alot less then ongui and can have some model data manipulation in it.
+        ///     This is also were any hotkeys are intercepted.
+        /// </summary>
+        void Update()
+        {
+        }
+
+        /// <summary>
+        ///     called anywhere from 60 times a sec to 1000 times a second. Only display GUI in this function. 
+        ///     No model data should built/manipulated in this function.
+        /// </summary>
+        void OnGui()
+        {
+            if (_showErrorDialog) {
+                DisplayErrorDialog();
+                UpdateTimer.Start();
+            }
+        }
+
+        /// <summary>
+        ///     Basic function for displaying a simple "toast" error message.
+        ///     This message is on a timer for 5 seconds by default.
+        /// </summary>
+        /// <param name="error">
+        ///     The string the reprosents the error message that needs to be dispalyed.
+        /// </param>
+        public static void DisplayErrorMessage(string error)
+        {
+            DisplayMessage("An Error has Occurred", error);
+        }
+
+        /// <summary>
+        ///     A simple function to display a message on the screen. 
+        ///     This message is on a timer for 5 seconds by default.
+        /// </summary>
+        /// <param name="title">
+        ///     The title of the message to display.
+        /// </param>
+        /// <param name="body">
+        ///     The body of the message to display.
+        /// </param>
+        public static void DisplayMessage(string title, string body)
+        {
+            _messageTitle = title;
+            _message = body;
+            _showErrorDialog = true;
+        }
+        
+        //This will stop the display from showing when the timer runs out.
+        private void UpdateDisplay(object sender, ElapsedEventArgs e)
+        {
+            _showErrorDialog = false;
+            UpdateTimer.Stop();
+        }
+
+        //This will display the error dailog box.
+        private void DisplayErrorDialog()
+        {
+            Rect displayErrorRect = new Rect(Screen.width / 2 - 160, Screen.height - 130, 320, 120);
+            guiMgr.DrawWindow(displayErrorRect, _messageTitle, false);
+            displayErrorRect.y += 50;
+            displayErrorRect.height += 50;
+            guiMgr.DrawTextCenteredWhite(displayErrorRect, _message);
+        }
+    }
+}
---
+++ MapStatsChooser/MapStatsChooserPlugin.cs
@@ -0,0 +1,38 @@
+﻿
+using Timber_and_Stone.API;
+using Timber_and_Stone.API.Event;
+using Timber_and_Stone.Event;
+
+namespace Plugin.KTMBobisback.MapStatsChooser
+{
+    /// <summary>
+    ///     The main plugin for this mod. It is called by the game through predefined functions.
+    /// </summary>
+    public class MapStatsChooserPlugin : CSharpPlugin, IEventListener
+    {
+        /// <summary>
+        ///     This function is called when the mod loads at startup
+        /// </summary>
+        public override void OnLoad() {
+            GUIManager.getInstance().AddTextLine("KTM & Bobisback Map Stat Chooser Loaded");
+            //add all of our GUI's to the game
+            GUIManager.getInstance().gameObject.AddComponent(typeof(GUIWindowMapStatsChooser));
+        }
+
+        /// <summary>
+        ///     This function is also called on mod startup. It is called after OnLoad
+        /// </summary>
+        public override void OnEnable() {
+            GUIManager.getInstance().AddTextLine("KTM & Bobisback Map Stat Chooser Enabled");
+            EventManager.getInstance().Register(this);
+        }
+
+        /// <summary>
+        ///     Becuase OnDisable() is not called at the moment, I use this function to save any settings before my mod gets unloaded. 
+        ///     This fucntion is called a deconstructor, it is called when the class is being deallocated from memory.
+        /// </summary>
+        ~MapStatsChooserPlugin() {
+            //SettingsManager.SaveSettings();
+        }
+    }
+}
